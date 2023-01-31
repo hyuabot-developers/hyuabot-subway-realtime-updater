150bacbf1383e2658a0e913afe3424ed748d750e
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-01-31 16:56:47 +0900

    feat: Master 노드의 데이터베이스를 활용할 수 있게 수정

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import asyncio
 
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import sessionmaker
 
 from scripts.realtime import get_realtime_data
-from utils.database import get_db_engine
+from utils.database import get_db_engine, get_master_db_engine
 
 
 async def main():
@@ -12,7 +13,20 @@ async def main():
     session = session_constructor()
     if session is None:
         raise RuntimeError("Failed to get db session")
-    job_list = [get_realtime_data(session, 1004, "4호선"), get_realtime_data(session, 1071, "수인분당선")]
+    try:
+        await execute_script(session)
+    except OperationalError:
+        connection = get_master_db_engine()
+        session_constructor = sessionmaker(bind=connection)
+        session = session_constructor()
+        if session is None:
+            raise RuntimeError("Failed to get db session")
+        await execute_script(session)
+
+
+async def execute_script(session):
+    job_list = [get_realtime_data(session, 1004, "4호선"),
+                get_realtime_data(session, 1071, "수인분당선")]
     await asyncio.gather(*job_list)
     session.close()
 
